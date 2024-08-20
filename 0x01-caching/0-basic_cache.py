bef77e43f4a39c8b1cc68268975745f6cbb87170
imoleFolio <dremkay71@gmail.com>
2024-08-20 10:48:33 +0100

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""
+Create a class BasicCache that inherits from BaseCaching and is a
+caching system:
+
+You must use self.cache_data - dictionary from the parent class BaseCaching
+This caching system doesn’t have limit
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the item value for the key key.
+If key or item is None, this method should not do anything.
+def get(self, key):
+Must return the value in self.cache_data linked to key.
+If key is None or if the key doesn’t exist in self.cache_data, return None.
+"""
+
+
+class BaseCaching():
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        raise NotImplementedError(
+            "put must be implemented in your cache class"
+        )
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        raise NotImplementedError(
+            "get must be implemented in your cache class"
+        )
+
+
+class BasicCache(BaseCaching):
+    """Implements basicCahe, """
+    def __init__(self):
+        """initialize instance of the class"""
+        super().__init__()
+        self.MAX_ITEMS = None
+
+    def put(self, key, item):
+        """insert the value in cache"""
+        if key and item:
+            self.cache_data.update([(key, item)])
+
+    def get(self, key):
+        """get the value of key in cache"""
+        return self.cache_data.get(key, None)
+
+
+if __name__ == "__main__":
+    my_cache = BasicCache()
+    my_cache.print_cache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    print(my_cache.get("D"))
+    my_cache.print_cache()
+    my_cache.put("D", "School")
+    my_cache.put("E", "Battery")
+    my_cache.put("A", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
